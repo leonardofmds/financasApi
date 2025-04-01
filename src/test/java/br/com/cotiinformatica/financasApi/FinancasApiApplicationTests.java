374fefa0c26e3842ef36ee3304b46f7365f6a51a
Leonardo Menezes <leonardomenezes.dev@gmail.com>
2025-04-01 13:20:32 -0300

    Update FinancasApiApplicationTests.java

--- src/test/java/br/com/cotiinformatica/financasApi/FinancasApiApplicationTests.java
+++ src/test/java/br/com/cotiinformatica/financasApi/FinancasApiApplicationTests.java
@@ -3,11 +3,59 @@ package br.com.cotiinformatica.financasApi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
 @SpringBootTest
 class FinancasApiApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void cadastrarTipoTest() {
+		fail("Not yet implemented");
 	}
+
+	@Test
+	void atualizarTipoTest() {
+		fail("Not yet implemented");
+	}
+	@Test
+	void excluirTipoTest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void consultarTiposTest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void obterTipoTest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void cadastrarContaTest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void atualizarContaTest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void excluirContaTest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void consultarContasTest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void obterContaTest() {
+		fail("Not yet implemented");
+	}
+
 
 }
